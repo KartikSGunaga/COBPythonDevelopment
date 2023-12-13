a236dda21380dbea81cd9f03367e359f543e15c3
KartikSGunaga <kartik.s@learner.manipal.edu>
2023-12-13 18:47:15 +0530

    13Dec-02 commits

--- task02/PasswordManager.py
+++ task02/PasswordManager.py
@@ -1,15 +1,17 @@
+# Importing necessary modules
 import random as rd
 import string as st
 from ecdsa import SigningKey, NIST256p, BadSignatureError
 from hashlib import sha256
 from ecdsa.util import sigencode_der, sigdecode_der
 
+# Class to generate and manage passwords
 class PasswordGenerator:
     def __init__(self):
-        self.passwordList = []
+        self.passwordList = []  # List to store generated passwords
 
-    def generatePassword(self, length, numLength, spCharLength,
-                         upperCaseLength, lowerCaseLength):
+    # Method to generate a random password based on specified criteria
+    def generatePassword(self, length, numLength, spCharLength, upperCaseLength, lowerCaseLength):
         count = 0
         password = ""
         userName = input("\nEnter the username you wish to associate the password with: ")
@@ -30,17 +32,17 @@ class PasswordGenerator:
                 count += 1
 
         print(f"The recommended password is: {password}")
-        self.passwordList.append({"username": userName,
-                              "Password": password})
+        self.passwordList.append({"username": userName, "Password": password})
 
+    # Method to specify the length of the password based on user input
     def specifyPasswordLength(self):
         choice = input("\nDo you wish to define password length?(yes or no): ")
 
         if choice == "yes":
             length = int(input("\nEnter the password length: "))
             numLength = int(input(f"\nEnter the number of digits you want({length} chars remaining): "))
-            spCharLength = int(input(f"\nEnter the number of digits you want({length - numLength} chars remaining): "))
-            upperCaseLength = int(input(f"\nEnter the number of digits you want({length - numLength - spCharLength} chars remaining): "))
+            spCharLength = int(input(f"\nEnter the number of special characters you want({length - numLength} chars remaining): "))
+            upperCaseLength = int(input(f"\nEnter the number of uppercase characters you want({length - numLength - spCharLength} chars remaining): "))
             lowerCaseLength = length - spCharLength - upperCaseLength - numLength
 
         else:
@@ -50,54 +52,56 @@ class PasswordGenerator:
             upperCaseLength = rd.randint(1, length - numLength - spCharLength - 1)
             lowerCaseLength = length - spCharLength - upperCaseLength - numLength
 
-        return length, numLength, spCharLength,upperCaseLength, lowerCaseLength
+        return length, numLength, spCharLength, upperCaseLength, lowerCaseLength
 
+    # Method to display a password associated with a specific username
     def displayPasswordByUsername(self, username):
-        bool = False
-        for key, value in self.passwordList:
-            if key.lower() == username:
-                print(f'\nPassword associated with \"{key}\" is: {value} ')
-                bool = True
+        found = False
+        for entry in self.passwordList:
+            if entry["username"].lower() == username.lower():
+                print(f'\nPassword associated with \"{entry["username"]}\" is: {entry["Password"]} ')
+                found = True
 
-        if not bool:
+        if not found:
             print("\nNo such username available.")
 
+    # Method to display all stored passwords
     def displayAllPasswords(self):
         for password in self.passwordList:
             print("\n", password)
 
+# Class to handle authentication and password encryption/decryption
 class Authenticate:
     def __init__(self, password):
         self.privateKey = SigningKey.generate(curve=NIST256p)
         self.publicKey = self.privateKey.verifying_key
         self.password = password
 
-    # def generateKeyPair(self):
-    #     self.privateKey = SigningKey.generate(curve=NIST256p)
-    #     self.publicKey = self.privateKey.verifying_key
-
+    # Method to save generated private and public key pairs to files
     def saveKeyPairs(self):
-        with open("privateKey.pem","wb") as file:
+        with open("privateKey.pem", "wb") as file:
             file.write(self.privateKey.to_pem(format="pkcs8"))
 
         with open("publicKey.pem", "wb") as file:
             file.write(self.publicKey.to_pem(format="pkcs8"))
+
+    # Method to encrypt a password using private key
     def encryptPassword(self):
-        encryptedPassword = self.publicKey.sign_deterministic(
+        encryptedPassword = self.privateKey.sign_deterministic(
             self.password,
             hashfunc=sha256,
             sigencode=sigencode_der
         )
         return encryptedPassword
 
+    # Method to decrypt an encrypted password using public key
     def decryptPassword(self, encryptedPassword):
-        decryptedPassword = self.privateKey.verify(encryptedPassword,
-                                                   self.password,
-                                                   sha256,
-                                                   sigdecode=sigdecode_der)
+        decryptedPassword = self.publicKey.verify(encryptedPassword,
+                                                  self.password,
+                                                  sha256,
+                                                  sigdecode=sigdecode_der)
 
         try:
-            # ret = self.privateKey.verify(decryptedPassword, self.password, sha256, sigdecode=sigdecode_der)
             assert decryptedPassword
             print("Valid signature")
         except BadSignatureError:
@@ -105,6 +109,7 @@ class Authenticate:
 
         return decryptedPassword
 
+# Method to display the menu options
 def menu():
     print("\n  Menu\n"
           "1. Generate Password \n"
@@ -112,12 +117,14 @@ def menu():
           "3. Retrieve password by username\n"
           "4. Exit")
 
+# Method to authenticate the user and perform actions based on menu choice
 def authenticate(password):
     user = Authenticate(password)
     encryptData = user.encryptPassword()
 
     return user.decryptPassword(encryptData)
 
+# Main method to run the password manager
 def main():
     print("\nWelcome to Kartik's Password Manager!")
     menu()
@@ -154,8 +161,7 @@ def main():
                 break
 
         except ValueError:
-                print("\nBad choice. Please input integers between 1-5.")
-
+            print("\nBad choice. Please input integers between 1-5.")
 
 if __name__ == "__main__":
     main()
