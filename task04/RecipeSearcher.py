97e440933d82d1726ad5fa91851e94d0e6f1fe4a
KartikSGunaga <kartik.s@learner.manipal.edu>
2023-12-29 17:35:19 +0530

    29Dec commits: Recipe Searcher complete

--- task04/RecipeSearcher.py
+++ task04/RecipeSearcher.py
@@ -1,6 +1,6 @@
 import json, requests
 
-# bc29c01c4c2b478ea4a4dcdbc26908e2
+
 class Recipes:
     def __init__(self):
         pass
@@ -17,8 +17,8 @@ class Recipes:
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
 
-    def getResponse(self,ingredients, numOfRecipes, rank):
-        url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&number={numOfRecipes}&ranking={rank}"
+    def getResponse(self, ingredients, numOfRecipes, rank, apiKey):
+        url = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={apiKey}&ingredients={ingredients}&number={numOfRecipes}&ranking={rank}"
 
         try:
             response = requests.get(url)
@@ -30,16 +30,16 @@ class Recipes:
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
 
+
 def getParameters():
-    response = None
-    ingredients = []
+    ingredientsList = []
     count = 0
     while True:
         ingredient = input("\nEnter the ingredient: ").lower().strip()
         if count == 0:
-            ingredients.append(f"{ingredient}")
+            ingredientsList.append(f"{ingredient}")
         else:
-            ingredients.append(f"+{ingredient}")
+            ingredientsList.append(f"+{ingredient}")
 
         while True:
             try:
@@ -47,7 +47,7 @@ def getParameters():
                 if response in ['y', 'n']:
                     break
             except:
-                print("\nPlease input either yes or no:")
+                print("\nPlease input either y or n: ")
 
         if response == 'n':
             break
@@ -55,8 +55,10 @@ def getParameters():
     numOfRecipesRequired = int(input("\nEnter the number of recipes you need (max: 100): "))
 
     rank = int(input("""
-            Press 1 to maximize available ingredients
-            Press 2 to minimize missing ingredients: """))
+Press 1 to maximize available ingredients
+Press 2 to minimize missing ingredients: """))
+
+    ingredients = ",".join(ingredientsList)
 
     return ingredients, numOfRecipesRequired, rank
 
@@ -65,11 +67,20 @@ def main():
     recipes = Recipes()
     print("\nWelcome to Kartik's Recipe Finder!")
 
-    apiKey = input("\nEnter the API Key:(bc29c01c4c2b478ea4a4dcdbc26908e2): ")
-    recipes.authenticate(apiKey)
+    apiKey = input("\nEnter the API Key: ")
 
-    #ingredients, numOfRecipes, rank = getParameters()
-    #print(recipes.getResponse(ingredients, numOfRecipes, rank))
+    while True:
+        count = 1
+        ingredients, numOfRecipes, rank = getParameters()
+        recipesFound = recipes.getResponse(ingredients, numOfRecipes, rank, apiKey)
+        print("\nBelow are some recipes you could try: ")
+        for item in recipesFound:
+            print(f"{count}. {item}")
+            count += 1
+
+        again = input("\nDo you wish to search for recipes again?(y/n): ").lower()
+        if again == 'n':
+            break
 
 
 if __name__ == '__main__':
